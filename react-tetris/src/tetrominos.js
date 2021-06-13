80a028161adb8b4bf7f01e4bba292b171536f643
Liz <lee.anewed@gmail.com>
2021-06-12 20:33:51 -0500

    Update tetrominos.js

--- react-tetris/src/tetrominos.js
+++ react-tetris/src/tetrominos.js
@@ -6,7 +6,7 @@ export const TETROMINOS = {
 			[0, 'I', 0, 0],
 			[0, 'I', 0, 0],
 			[0, 'I', 0, 0],
-		]
+		],
 		color: '80, 227, 230',
 	},
 	J: {
@@ -14,7 +14,7 @@ export const TETROMINOS = {
 			[0, 'J', 0],
 			[0, 'J', 0],
 			['J', 'J', 0],
-		]
+		],
 		color: '36, 95, 223',
 	},
 	L: {
@@ -22,14 +22,14 @@ export const TETROMINOS = {
 			[0, 'L', 0],
 			[0, 'L', 0],
 			[0, 'L', 'L'],
-		]
+		],
 		color: '223, 173, 36',
 	},
 	O: {
 		shape: [
 			['O', 'O'],
 			['O', 'O'],
-		]
+		],
 		color: '223, 217, 36',
 	},
 	S: {
@@ -37,7 +37,7 @@ export const TETROMINOS = {
 			[0, 'S', 'S'],
 			['S', 'S', 0],
 			[0, 0, 0],
-		]
+		],
 		color: '48, 211, 56',
 	},
 	T: {
@@ -45,7 +45,7 @@ export const TETROMINOS = {
 			[0, 0, 0],
 			['T', 'T', 'T'],
 			[0, 'T', 0],
-		]
+		],
 		color: '132, 61, 198',
 	},
 	Z: {
@@ -53,7 +53,7 @@ export const TETROMINOS = {
 			['Z', 'Z', 0],
 			[0, 'Z', 'Z'],
 			[0,  0, 0],
-		]
+		],
 		color: '227, 78, 78',
 	},
 }
