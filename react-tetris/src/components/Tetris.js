cfd9584da4fe8d773669315d2b087d4ed638d608
Liz <lee.anewed@gmail.com>
2021-06-16 09:10:11 -0500

    Added styled components for Cell, Stage, and Tetris components

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 //Components
 import Stage from './Stage';
 import Display from './Display';
@@ -8,17 +9,19 @@ import StartButton from './StartButton';
 const Tetris = () => {
 
 	return(
-		<div>
-			<Stage stage={createStage()} />
-			<aside>
-				<div>
-					<Display text="Score" />
-					<Display text="Rows" />
-					<Display text="Level" />
-				</div>
-				<StartButton />
-			</aside>
-		</div>	
+		<StyledTetrisWrapper>
+			<StyledTetris>
+				<Stage stage={createStage()} />
+				<aside>
+					<div>
+						<Display text="Score" />
+						<Display text="Rows" />
+						<Display text="Level" />
+					</div>
+					<StartButton />
+				</aside>
+			</StyledTetris>
+		</StyledTetrisWrapper>	
 	)
 }
 
